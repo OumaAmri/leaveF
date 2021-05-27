e85677da6f174f3653a42d6c7b6ec8fa8ee6d088
Oumayma AMRI <Oumayma.AMRI.STG@Teal.ma>
2021-05-27 16:28:20 -0400

    First commit

---
+++ leave-management/Data/Employee.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Matricule { get; set; }
+        public string Tower { get; set; }
+        public string Manager { get; set; }
+
+
+        public DateTime StartDate { get; set; }
+
+
+        public DateTime EndDate { get; set; }
+
+
+        public DateTime EnboardingDate { get; set; }
+        public string JobTitle { get; set; }
+
+        public string SkillSet { get; set; }
+        public string MainSpeciality { get; set; }
+        public string Certification { get; set; }
+        public string OtherRequiredSkill { get; set; }
+        public string Provider { get; set; }
+        public string SubcoName { get; set; }
+
+
+        public DateTime CreationDate { get; set; }
+
+        public string Rate { get; set; }
+    }
+}
---
+++ leave-management/Data/Migrations/20210525003951_migration1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leave_management.Data.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TaxId",
+                table: "AspNetUsers",
+                newName: "Tower");
+
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                newName: "StartDate");
+
+            migrationBuilder.RenameColumn(
+                name: "DateJoined",
+                table: "AspNetUsers",
+                newName: "EndDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Certification",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EnboardingDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "JobTitle",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MainSpeciality",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Manager",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Matricule",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherRequiredSkill",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Provider",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rate",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SkillSet",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SubcoName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Certification",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "EnboardingDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "JobTitle",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MainSpeciality",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Manager",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Matricule",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OtherRequiredSkill",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Provider",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Rate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "SkillSet",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "SubcoName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Tower",
+                table: "AspNetUsers",
+                newName: "TaxId");
+
+            migrationBuilder.RenameColumn(
+                name: "StartDate",
+                table: "AspNetUsers",
+                newName: "DateOfBirth");
+
+            migrationBuilder.RenameColumn(
+                name: "EndDate",
+                table: "AspNetUsers",
+                newName: "DateJoined");
+        }
+    }
+}
